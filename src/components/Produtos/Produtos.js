b691e8f85300e2c9071c693da1767d4664e2ea0a
joicefonseca <joicefonseca@unipam.edu.br>
2018-01-31 01:00:59 -0200

    excluir produtos

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -7,11 +7,55 @@ export default {
     },
     data(){
         return {
+            detalhesProdutos: false,
+            valoresDetalhesProdutos: {},
+            formaCompra: false,
+            valorProduto: null,
+            pagamento1: "",
+            pagamento2: "",
+            pagamento3: "",
+            parcelas: ['1 x', '2 x', '3 x' , '4 x', '5 x' ],
+            parcela1x: 0,
+            parcela2x: 0,
+            parcela3x: 0,
+            parcela4x: 0,
+            parcela5x: 0,
+            
+        }
+    },
+    watch:{
+        pagamento3:function(){
+            console.log(this.pagamento3)
+            console.log(this.valorProduto)
         }
     },
     methods: {
         cadastrarProdutos(){
             this.$router.push("/cadastro")
         },
+        excluirProduto(item){
+            tabelaProdutos.child(item).remove()
+        },
+        detalhesProduto(item){
+            console.log(item)
+            this.valoresDetalhesProdutos=item
+            this.detalhesProdutos= true
+        },
+        comprarProduto(item){
+           
+            this.valorProduto= item
+            this.formaCompra= true
+            Number(item)
+            var juros1= Number(item)*0.02
+            this.parcela1x= Number(item)+juros1
+            console.log(this.parcela1x)
+            this.parcelas= ['1 x ' + 'R$'+ this.parcela1x , '2 x', '3 x' , '4 x', '5 x' ]
+            
+            
+        }
+
+
+        
+
     }
 }
--- src/components/Produtos/Produtos.vue
+++ src/components/Produtos/Produtos.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-md fluid>
   <v-layout row wrap>
-    <v-flex xs6 v-for="item in produtos" :key= "item.id">
+    <v-flex xs6 v-for="item in produtos" :key="item['.key']">
       <v-card>
         <v-card-media src="static/pc1.jpg" height="250px" width="200px">
         </v-card-media>
@@ -15,13 +15,81 @@
           </div>
         </v-card-text>
         <v-card-actions xs6>
-          <v-btn flat color="pink">Mais Informações</v-btn>
-          <v-btn flat color="pink">Comprar</v-btn>
+          <v-btn flat color="pink" @click="detalhesProduto(item)">Mais Informações</v-btn>
+          <v-btn flat color="pink" @click="comprarProduto(item.preco)">Comprar</v-btn>
           <v-btn flat color="pink">Editar</v-btn>
-          <v-btn flat color="pink">Excluir</v-btn>
+          <v-btn flat color="pink" @click="excluirProduto(item['.key'])">Excluir</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
+    <v-dialog v-model="detalhesProdutos" width="50%">
+        <v-card>
+          <v-card-text>
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-and flex-box>
+                  <p>
+                    <b>Nome: </b>{{valoresDetalhesProdutos.nome}}
+                  </p>
+                  <p>
+                    <b>Preço: R$</b>{{valoresDetalhesProdutos.preco}}
+                  </p>
+                  <p>
+                    <b>Detalhes: </b>{{valoresDetalhesProdutos.descricao}}
+                  </p>
+                  <p>
+                    <b>Dados Técnicos: </b>{{valoresDetalhesProdutos.dadostecnicos}}
+                  </p>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click.stop="detalhesProdutos=false">Fechar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="formaCompra" width="50%">
+        <v-card>
+          <v-card-text>
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-and flex-box>
+                  <v-radio-group v-model="pagamento1" row>
+                    <v-radio label="Dinheiro" value="dinheiro"></v-radio>
+                    <v-radio label="Cartão" value="cartao"></v-radio>
+                  </v-radio-group>
+                  <v-radio-group v-model="pagamento2" row v-if="pagamento1=='cartao'">
+                    <v-radio label="À vista" value="avista"></v-radio>
+                    <v-radio label="À prazo" value="aprazo"></v-radio>
+                  </v-radio-group>
+                  <v-select :items="parcelas" v-model="pagamento3" row v-if="pagamento2=='aprazo'&& pagamento1=='cartao'"></v-select>
+                  <div v-if="pagamento2=='avista'&& pagamento1=='cartao'">
+                    <p>
+                      {{valorProduto}}
+                    </p>
+                  </div>
+                  <div v-if="pagamento1=='dinheiro'">
+                    <p>
+                      {{valorProduto-(valorProduto*0.1)}}
+                    </p>
+                  </div>
+                  <div>
+                    
+                  </div>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" block @click="finalizarCompra()">Finalizar Compra</v-btn>
+            <v-btn color="error" block @click.stop="formaCompra=false">Fechar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      
   </v-layout>
   </v-container>
 </template>
